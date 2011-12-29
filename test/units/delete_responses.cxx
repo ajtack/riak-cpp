499b4c6abc6be0756daf339a90375e23917275e9
Andres Jaan Tack <andres.jaan.tack@eesti.ee>
2011-12-29 04:24:44 +0100

    Completed DELETE requests up to previous levels.

--- test/units/delete_responses.cxx
+++ test/units/delete_responses.cxx
@@ -61,37 +61,51 @@ TEST_F(deleting_client, client_survives_trailing_data_with_RpbDelResp)
 }
 
 
-// TEST_F(riak_client_with_mocked_transport, client_accepts_well_formed_RbpDelResp)
-// {
-//     auto future = client->bucket("a").unmap("document");
+TEST_F(deleting_client, client_accepts_well_formed_RbpDelResp)
+{
+    client->delete_object("a", "document", response_handler);
     
-//     EXPECT_CALL(closure_signal, exercise());
-//     riak::message::wire_package clean_reply(riak::message::code::DeleteResponse, "");
-//     request_handler(std::error_code(), clean_reply.to_string().size(), clean_reply.to_string());
-//     ASSERT_TRUE(future.is_ready());
-//     ASSERT_TRUE(future.has_value());
-// }
+    EXPECT_CALL(closure_signal, exercise());
+    EXPECT_CALL(response_handler_mock, execute(
+            Eq(riak::make_server_error(riak::errc::no_error)),
+            Eq("a"),
+            Eq("document")));
+    EXPECT_CALL(sibling_resolution, evaluate(_)).Times(0);
+    riak::message::wire_package clean_reply(riak::message::code::DeleteResponse, "");
+    request_handler(std::error_code(), clean_reply.to_string().size(), clean_reply.to_string());
+
+}
 
 
-// TEST_F(riak_client_with_mocked_transport, client_accepts_well_formed_unmap_response_in_parts)
-// {
-//     auto future = client->bucket("a").unmap("document");
+TEST_F(deleting_client, client_accepts_well_formed_unmap_response_in_parts)
+{
+    EXPECT_CALL(sibling_resolution, evaluate(_)).Times(0);
+    client->delete_object("a", "document", response_handler);
     
-//     std::string response_data;
-//     std::string canned_delete_response = "";   // Deletes are only message code responses.
-//     riak::message::wire_package clean_reply(riak::message::code::DeleteResponse, canned_delete_response);
-//     auto data = clean_reply.to_string();
-//     assert(data.size() >= 2);
-//     auto first_half = data.substr(0, data.size() / 2);
-//     auto second_half = data.substr(data.size() / 2, data.size() - first_half.size());
-//     request_handler(std::error_code(), first_half.size(), first_half);
-//     ASSERT_TRUE(not future.is_ready());
+    std::string response_data;
+    std::string canned_delete_response = "";   // Deletes are only message code responses.
+    riak::message::wire_package clean_reply(riak::message::code::DeleteResponse, canned_delete_response);
+    auto data = clean_reply.to_string();
+    assert(data.size() >= 2);
+    auto first_half = data.substr(0, data.size() / 2);
+    auto second_half = data.substr(data.size() / 2, data.size() - first_half.size());
 
-//     EXPECT_CALL(closure_signal, exercise());
-//     request_handler(std::error_code(), second_half.size(), second_half);
-//     ASSERT_TRUE(future.is_ready());
-//     ASSERT_TRUE(future.has_value());
-// }
+    // The first half should be correctly buffered.
+    EXPECT_CALL(closure_signal, exercise()).Times(0);
+    EXPECT_CALL(response_handler_mock, execute(
+            Eq(riak::make_server_error(riak::errc::no_error)),
+            Eq("a"),
+            Eq("document"))).Times(0);
+    request_handler(std::error_code(), first_half.size(), first_half);
+
+    // The second half should trigger a response callback.
+    EXPECT_CALL(closure_signal, exercise());
+    EXPECT_CALL(response_handler_mock, execute(
+            Eq(riak::make_server_error(riak::errc::no_error)),
+            Eq("a"),
+            Eq("document")));
+    request_handler(std::error_code(), second_half.size(), second_half);
+}
 
 //=============================================================================
     }   // namespace test
