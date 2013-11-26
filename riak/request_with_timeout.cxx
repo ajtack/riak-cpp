6616906ccc7d2f0d2362603cce1ac0432bf00f5a
Andres Jaan Tack <andres.jaan.tack@eesti.ee>
2013-11-26 02:37:30 +0200

    Unify a bunch of whitespace (prefer tabs -- they're just better).

--- riak/request_with_timeout.cxx
+++ riak/request_with_timeout.cxx
@@ -12,71 +12,73 @@ using std::placeholders::_3;
 
 
 request_with_timeout::request_with_timeout (
-        const std::string& data,
-        std::chrono::milliseconds timeout,
-        message::buffering_handler& h,
-        boost::asio::io_service& ios)
-  : timeout_length_(timeout),
-    timeout_(ios),
-    request_data_(data),
-    response_callback_(h),
-    succeeded_(false),
-    timed_out_(false)
+		const std::string& data,
+		std::chrono::milliseconds timeout,
+		message::buffering_handler& h,
+		boost::asio::io_service& ios)
+  : timeout_length_(timeout)
+  , timeout_(ios)
+  , request_data_(data)
+  , response_callback_(h)
+  , succeeded_(false)
+  , timed_out_(false)
 {   }
 
 
 void request_with_timeout::dispatch_via (transport::delivery_provider& deliver)
 {
-    // The request can only be sent once.
-    assert(not terminate_request_ and not succeeded_ and not timed_out_);
-    auto on_response = std::bind(&request_with_timeout::on_response, shared_from_this(), _1, _2, _3);
-    terminate_request_ = deliver(request_data_, on_response);
+	// The request can only be sent once.
+	assert(not terminate_request_ and not succeeded_ and not timed_out_);
+	auto on_response = std::bind(&request_with_timeout::on_response, shared_from_this(), _1, _2, _3);
+	terminate_request_ = deliver(request_data_, on_response);
 
-    timeout_.expires_from_now(boost::posix_time::milliseconds(timeout_length_.count()));
-    auto on_timeout = std::bind(&request_with_timeout::on_timeout, shared_from_this(), _1);
-    timeout_.async_wait(on_timeout);
+	timeout_.expires_from_now(boost::posix_time::milliseconds(timeout_length_.count()));
+	auto on_timeout = std::bind(&request_with_timeout::on_timeout, shared_from_this(), _1);
+	timeout_.async_wait(on_timeout);
 }
 
 
 void request_with_timeout::on_response (std::error_code error, size_t bytes_received, const std::string& raw_data)
 {
-    assert(not succeeded_ and (!! terminate_request_));
-    unique_lock<mutex> serialize(this->mutex_);
+	assert(not succeeded_ and (!! terminate_request_));
+	unique_lock<mutex> serialize(this->mutex_);
 
-    // Whatever happened, it constitutes activity. Stop the timeout timer.
-    timeout_.cancel();
+	// Whatever happened, it constitutes activity. Stop the timeout timer.
+	timeout_.cancel();
 
-    bool error_condition = (timed_out_ or error);
-    if (not error_condition) {
-        if (response_callback_(std::error_code(), bytes_received, raw_data)) {
-            (*terminate_request_)(false);
-            succeeded_ = true;
-	    terminate_request_.reset();
-        } else {
-            timeout_.expires_from_now(boost::posix_time::milliseconds(timeout_length_.count()));
-            auto on_timeout = std::bind(&request_with_timeout::on_timeout, shared_from_this(), _1);
-            timeout_.async_wait(on_timeout);
-        }
-    } else {
-        (*terminate_request_)(true);
-        // Timeout already satisfied the response callback.
-        if (not timed_out_)
-            response_callback_(error, 0, raw_data);
-	terminate_request_.reset();
-    }
+	bool error_condition = (timed_out_ or error);
+	if (not error_condition) {
+		if (response_callback_(std::error_code(), bytes_received, raw_data)) {
+			(*terminate_request_)(false);
+			succeeded_ = true;
+			terminate_request_.reset();
+		} else {
+			timeout_.expires_from_now(boost::posix_time::milliseconds(timeout_length_.count()));
+			auto on_timeout = std::bind(&request_with_timeout::on_timeout, shared_from_this(), _1);
+			timeout_.async_wait(on_timeout);
+		}
+	} else {
+		(*terminate_request_)(true);
+
+		// Timeout already satisfied the response callback.
+		if (not timed_out_)
+			response_callback_(error, 0, raw_data);
+
+		terminate_request_.reset();
+	}
 }
 
 
 void request_with_timeout::on_timeout (const boost::system::error_code& error)
 {
-    assert(not timed_out_);
-    unique_lock<mutex> serialize(this->mutex_);
-    timed_out_ = not error;
-    if (timed_out_) {
-        auto timeout_error = std::make_error_code(std::errc::timed_out);
-        response_callback_(timeout_error, 0, "");
-	terminate_request_.reset();
-    }
+	assert(not timed_out_);
+	unique_lock<mutex> serialize(this->mutex_);
+	timed_out_ = not error;
+	if (timed_out_) {
+		auto timeout_error = std::make_error_code(std::errc::timed_out);
+		response_callback_(timeout_error, 0, "");
+		terminate_request_.reset();
+	}
 }
 
 //=============================================================================
--- riak/request_with_timeout.hxx
+++ riak/request_with_timeout.hxx
@@ -7,14 +7,14 @@
 #include <system_error>
 
 #ifdef _WIN32
-#include <boost/chrono.hpp>
-namespace std { namespace chrono = boost::chrono; }
+#	include <boost/chrono.hpp>
+	namespace std { namespace chrono = boost::chrono; }
 #else
-#include <chrono>
+#	include <chrono>
 #endif
 
 namespace boost {
-    namespace asio { class io_service; }
+	namespace asio { class io_service; }
 }
 
 //=============================================================================
@@ -22,40 +22,40 @@ namespace riak {
 //=============================================================================
 
 class request_with_timeout
-      : public std::enable_shared_from_this<request_with_timeout>
+    : public std::enable_shared_from_this<request_with_timeout>
 {
   public:
-    /*!
-     * Packages a task, but does not send it or begin timeout calculations.
-     * \param timeout is in milliseconds. It determines the maximum length of any radio silence
-     *     from the server.
-     */
-    request_with_timeout (
-            const std::string& data,
-            std::chrono::milliseconds timeout,
-            message::buffering_handler& h,
-            boost::asio::io_service& ios);
-    
-    /*!
-     * Sends the request and begins listening for a response asynchronously. Timeout countdown starts now.
-     * \pre There must exist a shared pointer to this request at the time of dispatch. It may be
-     *     reset as soon as the request is dispatched.
-     */
-    void dispatch_via (transport::delivery_provider& p);
+	/*!
+	 * Packages a task, but does not send it or begin timeout calculations.
+	 * \param timeout is in milliseconds. It determines the maximum length of any radio silence
+	 *        from the server.
+	 */
+	request_with_timeout (
+			const std::string& data,
+			std::chrono::milliseconds timeout,
+			message::buffering_handler& h,
+			boost::asio::io_service& ios);
+	
+	/*!
+	 * Sends the request and begins listening for a response asynchronously. Timeout countdown starts now.
+	 * \pre There must exist a shared pointer to this request at the time of dispatch. It may be
+	 *	 reset as soon as the request is dispatched.
+	 */
+	void dispatch_via (transport::delivery_provider& p);
 
   private:
-    void on_response (std::error_code, std::size_t, const std::string&);
-    void on_timeout (const boost::system::error_code&);
-      
-    mutable boost::mutex mutex_;
-    std::chrono::milliseconds timeout_length_;
-    boost::asio::deadline_timer timeout_;
-    message::buffering_handler response_callback_;
-    boost::optional<transport::option_to_terminate_request> terminate_request_;
-    const std::string request_data_;
-    
-    bool succeeded_;
-    bool timed_out_;
+	void on_response (std::error_code, std::size_t, const std::string&);
+	void on_timeout (const boost::system::error_code&);
+	  
+	mutable boost::mutex mutex_;
+	std::chrono::milliseconds timeout_length_;
+	boost::asio::deadline_timer timeout_;
+	message::buffering_handler response_callback_;
+	boost::optional<transport::option_to_terminate_request> terminate_request_;
+	const std::string request_data_;
+	
+	bool succeeded_;
+	bool timed_out_;
 };
 
 //=============================================================================
