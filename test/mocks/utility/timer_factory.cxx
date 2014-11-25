95fd71f5115b48a8c5497ed24124352db80df441
Andres Jaan Tack <andres.jaan.tack@eesti.ee>
2014-11-25 10:20:10 -0600

    Remove another lambda.

--- test/mocks/utility/timer_factory.cxx
+++ test/mocks/utility/timer_factory.cxx
@@ -5,15 +5,23 @@
 namespace riak {
 	namespace mock {
 		namespace utility {
+			namespace {
+//=============================================================================
+
+// Timeout behavior is distracting in the common case.
+mock::utility::timer* new_mock_timer () {
+	using namespace ::testing;
+	return new NiceMock<mock::utility::timer>;
+}
+
+//=============================================================================
+			}   // namespace (anonymous)
 //=============================================================================
 
 timer_factory::timer_factory ()
 {
 	using namespace ::testing;
-
-	// Timeout behavior is distracting in the common case.
-	auto new_mock_timer = []() { return new NiceMock<mock::utility::timer>; };
-	ON_CALL( *this, __create() ).WillByDefault(InvokeWithoutArgs(new_mock_timer));
+	ON_CALL( *this, __create() ).WillByDefault(InvokeWithoutArgs(&new_mock_timer));
 }
 
 // Defining this explicitly speeds up compilation time.
